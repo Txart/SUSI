1782c9b90c0f4861a29e8fea14965be9b2c32c1a
txart <iurzainki@gmail.com>
2025-11-19 10:50:49 +0100

    started using pydantic models

--- src/susi/config.py
+++ src/susi/config.py
@@ -1,7 +1,7 @@
-from dataclasses import dataclass
 from pathlib import Path
 import datetime
 import numpy as np
+from pydantic import BaseModel, DirectoryPath, FilePath, computed_field
 
 import susi.io.utils as io_utils
 
@@ -9,21 +9,19 @@ import susi.io.utils as io_utils
 project_root_path = io_utils.get_project_root()
 
 
-@dataclass
-class FilePaths:
-    input_folder: Path = project_root_path / Path("inputs/")
-    output_folder: Path = project_root_path / Path("outputs/")
+class FilePaths(BaseModel, strict=True):
+    input_folder: DirectoryPath = project_root_path / Path("inputs/")
+    output_folder: DirectoryPath = project_root_path / Path("outputs/")
 
-    weather_data_path: Path = project_root_path / Path("inputs/CFw.csv")
-
-    def __post_init__(self):
-        for path in (self.input_folder, self.output_folder, self.weather_data_path):
-            if not path.exists:
-                raise FileNotFoundError(f"File or folder {path} could not be found.")
+    weather_data_path: FilePath = project_root_path / Path("inputs/CFw.csv")
 
 
-@dataclass
-class SimulationParameters:
+class SimulationParameters(
+    BaseModel,
+    strict=True,
+    frozen=True,
+    arbitrary_types_allowed=True,  # This allows numpy arrays and other types which do not have built-in validation in Pydantic
+):
     # Time
     start_date: datetime.datetime  # Start date for simulation
     end_date: datetime.datetime  # End day for simulation
@@ -38,12 +36,17 @@ class SimulationParameters:
     initial_understorey_age_years: float
     site_fertility_class: int
 
-    def __post_init__(self):
+    @computed_field
+    @property
+    def n_computation_nodes_in_strip(self) -> int:
         # Number of computation nodes in the strip, 2-m width of node
-        self.n_computation_nodes_in_strip = int(self.strip_width_metres / 2)
+        return int(self.strip_width_metres / 2)
 
+    @computed_field
+    @property
+    def ageSim(self) -> dict[str, np.ndarray]:
         # Age of stand for all nodes along the strip
-        self.ageSim = {
+        return {
             "dominant": self.initial_dominant_stand_age_years
             * np.ones(self.n_computation_nodes_in_strip),
             "subdominant": self.initial_subdominant_stand_age_years
@@ -52,14 +55,16 @@ class SimulationParameters:
             * np.ones(self.n_computation_nodes_in_strip),
         }
 
+    @computed_field
+    @property
+    def sfc(self) -> np.ndarray:
         # site fertility class for all nodes along the strip
-        self.sfc = (
+        return (
             np.ones(self.n_computation_nodes_in_strip, dtype=int)
             * self.site_fertility_class
         )
 
 
-@dataclass
-class Config:
+class Config(BaseModel, strict=True):
     paths: FilePaths
     simulation_parameters: SimulationParameters
